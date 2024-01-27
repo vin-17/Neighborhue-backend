bcdfad89e93acbdc766a9d22a4b7da2f6298a5b4
Vineet Singh <vineet.yo3@gmail.com>
2024-01-28 01:36:23 +0530

    deploy fix1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 connectDB();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log("\n----------------------------------------------------------------------------------");
