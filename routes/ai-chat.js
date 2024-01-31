bbb65a07b8e8f4a0d78b56cb4924f06072b635ca
Vineet Singh <vineet.yo3@gmail.com>
2024-01-31 14:41:13 +0530

    token issue

--- routes/ai-chat.js
+++ routes/ai-chat.js
@@ -28,7 +28,7 @@ router.post("/chatbot", async (req, res) => {
           { role: 'user', content: `i want to shift at ${location} . please give all replies or suggestion to me text message - ${message} at ${location}. Give answer according to the context donot go beyond that.` },
         ],
         temperature: 1,
-        max_tokens: 1000,
+        max_tokens: 500,
       });
 
     // Extract chatbot's reply from the API response
