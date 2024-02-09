e56d268983cbba7be0452ceb33df2a9834ffa7c0
Vineet Singh <vineet.yo3@gmail.com>
2024-02-09 22:51:40 +0530

    cors issue f7.1

--- app.js
+++ app.js
@@ -23,39 +23,39 @@ const frontend_url = "https://neighborhue-frontend.vercel.app";
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://neighborhue-frontend.vercel.app/"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Private-Network", true);
-//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//   res.setHeader("Access-Control-Max-Age", 7200);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://neighborhue-frontend.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-//   next();
-// });
-
-app.use(cors({
-  origin: "https://neighborhue-frontend.vercel.app/",
-  methods: ["GET,PUT,PATCH,POST,DELETE"],
-  credentials: true,
-  // preflightContinue: true,
-}));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://neighborhue-frontend.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// app.use(cors({
+//   origin: "https://neighborhue-frontend.vercel.app",
+//   methods: ["GET,PUT,PATCH,POST,DELETE"],
+//   credentials: true,
+//   // preflightContinue: true,
+// }));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://neighborhue-frontend.vercel.app");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // app.options('*', cors());
 
